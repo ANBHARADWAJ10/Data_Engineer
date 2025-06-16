cdd9fa346d1e2684dd77b0f23a2f6db3681c672b
ANBHARADWAJ10 <nikhilbharadwaj2023@gmail.com>
2025-06-16 09:27:30 +0530

    Variables in python

---
+++ Variables and Datatypes/variables.py
@@ -0,0 +1,14 @@
+# Variables are used to store data in a program.
+# Varibles are esssential for data manipulation and storage.
+
+name = "Nikhil Bharadwaj" #string variable
+age = 23 #integer variable
+height = 5.7 #float variable
+is_student = False #boolean variable
+
+#printing the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("is_student:", is_student)
+
