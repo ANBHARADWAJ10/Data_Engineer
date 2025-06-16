bc1f03e179afd706d302017d08f527db9a7b49af
ANBHARADWAJ10 <nikhilbharadwaj2023@gmail.com>
2025-06-16 10:09:10 +0530

    moved everythin into python folder

---
+++ Python/Variables and Datatypes/variables.py
@@ -0,0 +1,13 @@
+# Varibles are esssential for data manipulation and storage.
+
+name = "Nikhil Bharadwaj" #string variable
+age = 23 #integer variable
+height = 5.7 #float variable
+is_student = False #boolean variable
+
+#printing the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("is_student:", is_student)
+
