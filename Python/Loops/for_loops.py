55a73a2509a25796ba41b9b425bb42ca7821d769
ANBHARADWAJ10 <nikhilbharadwaj2023@gmail.com>
2025-06-17 16:58:50 +0530

    added while_loops and for_loops

---
+++ Python/Loops/for_loops.py
@@ -0,0 +1,9 @@
+# for loops are used to iterate over a sequence (like a list, tuple, or string) or other iterable objects.
+# They are often used when the number of iterations is known beforehand.
+
+# Example: Print each character in a string
+name = "Nikhil"
+print(len(name))
+
+for char in name:
+    print("Character:", char, end=" ")
---
+++ Python/Loops/while_intro.py
@@ -0,0 +1,16 @@
+# while loop is used to execute a block of code repeatedly as long as a given condition is true.
+# It is often used when the number of iterations is not known beforehand.
+
+# Example: Print numbers from 1 to 5
+count = 1
+
+while count <= 5:
+    print("count:", count)
+    count += 1
+
+# Example: Print numbers from 5 to 1
+count1 = 5
+
+while count1 >= 1:
+    print("count1:", count1)
+    count1 -= 1
