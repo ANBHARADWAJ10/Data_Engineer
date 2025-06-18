a3d1cb429dca0592516d435efaacd316f548d503
ANBHARADWAJ10 <nikhilbharadwaj2023@gmail.com>
2025-06-18 11:17:20 +0530

    added exception handling

---
+++ Python/Exception Handling/exception_intro.py
@@ -0,0 +1,10 @@
+num = 0
+try:
+    div = 10 / num
+except Exception as e:
+    print('Error:', e)
+    print('You cannot divide by zero.')
+else:
+    print('Division successful:', div)
+finally:
+    print('completed')
